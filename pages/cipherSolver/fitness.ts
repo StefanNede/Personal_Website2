db1323812fc979ca69b4be990901e7a6eac56216
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-11 22:23:56 +0000

    added tetragram fitness test

--- pages/cipherSolver/fitness.ts
+++ pages/cipherSolver/fitness.ts
@@ -1,3 +1,6 @@
+import { formatString } from "./formatString"
+import {tetragrams} from "./data/tetragrams"
+
 export const bigramFitness = (text:string):number => {
     return 0.0
 }
@@ -6,6 +9,32 @@ export const trigramFitness = (text:string):number => {
     return 0.0
 }
 
+const getTetragrams = (text:string):Map<string, number> => {
+    text = formatString(text)
+    let tetras:Map<string,number> = new Map()
+    for (let i=0;i<text.length-4;i++) {
+        let tetra:string = text.substring(i,i+4)
+        if (tetras.has(tetra)) {
+            tetras.set(tetra, tetras.get(tetra)!+1)
+        } else {
+            tetras.set(tetra, 1)
+        }
+    }
+    return tetras
+}
+
 export const tetragramFitness = (text:string):number => {
-    return 0.0
+    let res = 0
+    let tetras = getTetragrams(text)
+    tetras.forEach((count, tetra) => {
+        let p:number = 0
+        if (tetragrams.has(tetra.toUpperCase())) { 
+            p = tetragrams.get(tetra.toUpperCase())
+        } else {
+            p = -10
+        }
+        console.log(p)
+        res += p
+    })
+    return res
 }
