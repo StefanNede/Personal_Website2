aecc4c2da7d5e9cfabf0e0e56fc08612b6203bcc
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-11 22:24:10 +0000

    now removes punctuation

--- pages/cipherSolver/formatString.ts
+++ pages/cipherSolver/formatString.ts
@@ -3,5 +3,7 @@ export const formatString = (text:string):string => {
     text = text.replace(' ','')
     text = text.replace(/\s+/g, '') //removes spaces 
     text = text.replace(/(\r\n|\n|\r)/gm, "") //removes line breaks
+    text= text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") // remove punctuation
+    text = text.replace(/\s{2,}/g," ") // remove spaces left over from punctuation
     return text
 } 
