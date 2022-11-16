54073db50915709a7b2c599f01e5be39cd7e9bad
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-16 23:31:04 +0000

    added base files for drawing

---
+++ components/drawingCanvas.tsx
@@ -0,0 +1,10 @@
+const DrawingCanvas:React.FC = () => {
+    return (
+        <div>
+            <canvas style={{border:"1px solid white", width:"80vw", height:"80vh"}}>
+
+            </canvas>
+        </div>
+    )
+}
+export default DrawingCanvas
--- pages/draw.tsx
+++ pages/draw.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link"
+import DrawingCanvas from "../components/drawingCanvas"
+import styles from '../styles/Draw.module.css'
 
 export default function Draw() {
     return (
         <div className="container">
-            <main>
-
-                <p>you can draw here</p>
+            <header className={styles.header}>
+                <h1>drawing app</h1>
+            </header>
+            <main className={styles.main}>
+                <div className={styles.options}>
+                    <button>download</button>
+                    <button>share</button>
+                </div>
+                <div className={styles.canvas}>
+                    <DrawingCanvas/>
+                </div>
             </main>
             <footer>
                 <Link href="/">back home</Link>
