2de3f8ccac7e6c97fce9c03967ab4f2e9cb52f05
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-11-27 10:03:15 +0000

    working rail fence decoder added

--- scripts/cipherSolver/railFence.ts
+++ scripts/cipherSolver/railFence.ts
@@ -32,42 +32,59 @@ const decode = (text:string, rails:number):string => {
         x += 1
         denominator = n + (n-1)*x
     }
-    // get y - FIX THIS - sometimes y can be 'negative' as in a diagonal going up is not fully filled
+
     while (numerator != denominator) {
         y += 1
         numerator = l + y
     }
         
-    let numDiagonals = x + 1
+    let numDiagonals:number = x + 1
+    let numTopElements:number = 0
+    let numBottomElements:number = 0
+    if (numDiagonals%2 == 0) {
+        // the last diagonal will be going upwards - so thre are n/2 elements on the bottom and n/2 + 1 on the top
+        numBottomElements = numDiagonals/2
+        numTopElements = numBottomElements+1
+    } else {
+        numTopElements = (numDiagonals+1)/2
+        numBottomElements = numTopElements
+    }
     // numDiagonals - 2 is the number of characters in the top and bottom row
     // numDiagonals is the number of characters in the middle rows
     let rows:string[] = []
     let previousIndex:number = 0
     for (let j=0;j<rails;j++) {
         if (j === 0) {
-            // FIX THIS
-            rows.push(text.substring(0, numDiagonals-2))
-            previousIndex = numDiagonals-2
-        } else if (j === rails-1) {
+            if (j < y && numDiagonals%2 == 0) {
+                rows.push(text.substring(0, numTopElements-1) + " ") 
+                previousIndex = numTopElements-1
+            } else {
+                rows.push(text.substring(0, numTopElements))
+                previousIndex = numTopElements
+            }
+        } else if (j === rails-1 && numDiagonals%2 == 1) {
             // apply the empty spaces 
-            if (j >= rails - y) {
+            if (j >= rails - y && numDiagonals%2 == 1) {
                 rows.push(text.substring(previousIndex, text.length) + " ")
             } else {
                 rows.push(text.substring(previousIndex, text.length))
             }
         } else {
             // apply the empty spaces 
-            if (j >= rails - y) {
-                rows.push(text.substring(previousIndex, text.length-2) + " ")
+            if (j >= rails - y && numDiagonals%2 == 1) {
+                rows.push(text.substring(previousIndex, previousIndex + numDiagonals-1) + " ")
                 previousIndex += numDiagonals-1
-            } else {
+            } else if (j < y && numDiagonals%2 == 0 ) {
+                rows.push(text.substring(previousIndex, previousIndex + numDiagonals-1) + " ")
+                previousIndex += numDiagonals-1
+            } 
+            else {
                 rows.push(text.substring(previousIndex, previousIndex + numDiagonals))
                 previousIndex += numDiagonals
             }
         }
     }
 
-    console.log(rows)
     let topCounter:number = 0
     let topHappened:boolean = false
     let bottomCounter:number = 0
@@ -119,17 +136,15 @@ const decode = (text:string, rails:number):string => {
 
 
 export const getRailfenceDecode = (text:string):any[] => {
-    // try rails and stop at the best bigram fitness - try 20 rails     
+    // try rails and stop at the best bigram fitness - try 30 rails     
     // alternatively use chi squared and when you get to a value similar to english stop, otherwise continue until 50 rails and return the best one
     let decoded:string = ""
     let bestFitness:number = bigramFitness(text)
     let key:number = 1
     text = formatString(text)
-    console.log(text)
     text = text.toUpperCase()
-    for (let i = 2; i<=20; i++) {
+    for (let i = 2; i<=40; i++) {
         let interDecoded:string = decode(text, i)
-        console.log(i, interDecoded)
         let interFitness:number = bigramFitness(interDecoded)
         if (interFitness > bestFitness) {
             decoded = interDecoded
