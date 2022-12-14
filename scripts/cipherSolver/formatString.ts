f44ed9454689c9f0e1d41cdbae039351578eb5a0
Stefan Nedelcu <stefan.n@gmx.co.uk>
2022-12-14 19:23:05 +0000

    fixed question mark bug

--- scripts/cipherSolver/formatString.ts
+++ scripts/cipherSolver/formatString.ts
@@ -3,7 +3,8 @@ export const formatString = (text:string):string => {
     text = text.replace(' ','')
     text = text.replace(/\s+/g, '') //removes spaces 
     text = text.replace(/(\r\n|\n|\r)/gm, "") //removes line breaks
-    text= text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") // remove punctuation
+    text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") // remove punctuation
+    text = text.replace(/\?/g,'') // have to hard code this for some reason
     text = text.replace(/\s{2,}/g," ") // remove spaces left over from punctuation
     return text
 } 
